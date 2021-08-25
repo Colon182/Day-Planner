1e4fe7162d9e8a85a4279ca324bad1e1f78ffae7
Christian Colon <christian.colon27@gmail.com>
2021-08-24 20:03:38 -0400

    shortened day planner to 5pm and added given classes to html from css starter code

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,4 +1,17 @@
 var timeDisplayEl = $("#currentDay")
+var nineAM = $("#hour-9");
+var tenAM = $("#hour-10");
+var elevenAM = $("#hour-11");
+var twelvePM = $("#hour-12");
+var onePM = $("#hour-1");
+var twoPM = $("#hour-2");
+var threePM = $("#hour-3");
+var fourPM = $("#hour-4");
+var fivePM = $("#hour-5");
+var sixPM = $("#hour-6");
+var sevenPM = $("#hour-7");
+var EightPM = $("#hour-8");
+
 
 function displayTime() {
     var rightNow = moment().format("MMM DD, YYYY");
@@ -6,3 +19,5 @@ function displayTime() {
 }
 
 displayTime();
+
+
