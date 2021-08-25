13ae31b8539da415cab4f3cce3c6e30be5ff8350
Christian Colon <christian.colon27@gmail.com>
2021-08-24 20:04:44 -0400

    adjusted javascript for 9-5pm day

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -8,9 +8,6 @@ var twoPM = $("#hour-2");
 var threePM = $("#hour-3");
 var fourPM = $("#hour-4");
 var fivePM = $("#hour-5");
-var sixPM = $("#hour-6");
-var sevenPM = $("#hour-7");
-var EightPM = $("#hour-8");
 
 
 function displayTime() {
