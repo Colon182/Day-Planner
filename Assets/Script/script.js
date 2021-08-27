42cfe2668a824fccb855a244ed19870e699b70cb
Christian Colon <christian.colon27@gmail.com>
2021-08-27 16:12:44 -0400

    switched id from hour- format to hours in military time

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,13 +1,4 @@
 var timeDisplayEl = $("#currentDay")
-var nineAM = $("#hour-9");
-var tenAM = $("#hour-10");
-var elevenAM = $("#hour-11");
-var twelvePM = $("#hour-12");
-var onePM = $("#hour-1");
-var twoPM = $("#hour-2");
-var threePM = $("#hour-3");
-var fourPM = $("#hour-4");
-var fivePM = $("#hour-5");
 
 
 function displayTime() {
@@ -16,4 +7,11 @@ function displayTime() {
 };
 displayTime();
 
-
+function colorSwap() {
+    // grabbing the current hours from moment.js
+    var presentTime = moment().hours();
+    // loop through time blocks to compare with presentTime variable
+    $('.time-block').each(function() {
+        var hourBlock = parseInt($(this).attr('id'))
+    })
+}
